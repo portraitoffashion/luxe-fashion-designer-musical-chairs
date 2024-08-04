4b82dea400da8765a5701d7605e8b6005e55f0d5
Yang Zhao <portr8offashion@gmail.com>
2024-08-04 21:12:59 +0800

    snake

--- src/components/Steps1.svelte
+++ src/components/Steps1.svelte
@@ -1,7 +1,7 @@
 <script>
     export let currentStep1;
     import Scrolly from "../helpers/Scrolly.svelte";
-    let intros=["The luxury fashion industry is a highly structured, hierarchical system, predominantly dominated by French and Italian brands.","Below, we explore some of the most prominent luxury fashion labels of our time, ranked by their estimated 2023 revenue","Despite the apparent diversity among these brands, many were acquired by major conglomerates throughout the 1980s and early 2000s. This acquisition spree formed a tiered matrix strategy that controls the supply chain, taste, and pricing across the entire industry."]
+    let intros=["The luxury fashion industry is a tapestry of structured hierarchy, dominated largely by iconic French and Italian brands. Let's delve into some of the most prominent players in this space, ranked in this pyramid graph by their estimated annual revenue.","Many of these illustrious brands began their journeys as small, independent ateliers. Over time, they have transformed into billion or even multi-billion dollar powerhouses.","Despite the apparent diversity, most of these brands have been swept up by major conglomerates like LVMH, Kering, and Richemont, especially during the acquisition waves of the 1980s and early 2000s. Out of the 18 designers highlighted, only four have maintained their independence.","These strategic acquisitions have crafted a matrixed approach that monopolizes nearly every facet of the market—from supply chains and talent acquisition to pricing strategies. Yet, even this seemingly invincible structure shows signs of strain."]
   </script>
   
   <div class="steps">
@@ -24,6 +24,8 @@
       display: flex;
       justify-content: center;
       place-items: center;
+      width: 50%;
+      margin: 0 auto;
     }
   
     .step.active {
