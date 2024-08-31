95a528356a7a86b2b9241745073a670e7cafbac6
Yang Zhao <portr8offashion@gmail.com>
2024-08-31 21:12:04 +0800

    intro

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -3,7 +3,9 @@
 
 </script>
 
-<p>The fashion industry is going through a lot of changes</p>
-<img src="../../assets/vicard.png" width=400 alt="Vicard">
+<p>The fashion industry is undergoing significant shifts. Recent news highlights sluggish sales and frequent designer turnovers, even at the highest echelons, sparking concern among industry observers. </p>
+<img src="../../assets/vicard.png" width=600 alt="Vicard">
+In June 2024, Virginie Viard parted ways with Chanel after 30 years of service, including five years as the artistic director of fashion collections.
+<img src="../../assets/michele.png" width=600 alt="Michele">
 <img src="../../assets/slimane.png" width=400 alt="Vicard">
 <img src="../../assets/galliano.png" width=400 alt="Vicard">
